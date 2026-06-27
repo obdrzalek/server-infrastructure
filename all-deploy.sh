5ad83c6221292b130bc31a3bffe4dc8a3818470b
Tomáš Obdržálek <tomas@obdrzalek.eu>
2026-06-27 18:08:47 +0200

    Fix paths in deployment scripts for consistency

--- all-deploy.sh
+++ all-deploy.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 ~/server-infrastructure/infra/deploy.sh start
-~/eduis/server-infrastructure/test/deploy.sh start
+~/eduis/infrastructure/test/deploy.sh start
 ~/server-infrastructure/nginx/deploy.sh start
--- all-stop.sh
+++ all-stop.sh
@@ -2,9 +2,5 @@
 set -euo pipefail
 
 ~/server-infrastructure/nginx/deploy.sh stop
-~/eduis/server-infrastructure/test/deploy.sh stop
+~/eduis/infrastructure/test/deploy.sh stop
 ~/server-infrastructure/infra/deploy.sh stop
-
-~/server-infrastructure/infra/deploy.sh start
-~/eduis/server-infrastructure/test/deploy.sh start
-~/server-infrastructure/nginx/deploy.sh start
