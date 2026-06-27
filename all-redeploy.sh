bf118b2974504fd2b996a2acbcf131b939b8522a
Tomáš Obdržálek <tomas@obdrzalek.eu>
2026-06-27 17:49:49 +0200

    x

---
+++ all-redeploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+~/server-infrastructure/all-stop.sh
+~/server-infrastructure/all-deploy.sh
--- deploy-all.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-~/server-infrastructure/infra/deploy.sh start
-~/eduis/server-infrastructure/test/deploy.sh start
-~/server-infrastructure/nginx/deploy.sh start
